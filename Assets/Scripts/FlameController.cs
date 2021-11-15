3fa53ef7191d292d6c8ad243fd3dd380308cadd1
Emily Bender <bender.emily1@gmail.com>
2021-11-14 21:25:00 -0500

    Flames go out for specified time before relighting again

--- Assets/Scripts/FlameController.cs
+++ Assets/Scripts/FlameController.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum FlameState
+{
+    Lit, 
+    Nonlit
+}
+
 public class FlameController : MonoBehaviour
 {
     [Header("Data")]
@@ -10,14 +16,25 @@ public class FlameController : MonoBehaviour
     [Header("Broadcasting on channels")]
     [SerializeField] private FlameEventChannelSO updateFlameStaminaEvent = default;
 
+    public float timeTilFlameRelights = 10f;
+
     private PlayerController playerController;
+
+    private Animator animator;
+
+    private FlameState currentFlameState;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        currentFlameState = FlameState.Lit;
+    }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             playerController = collision.gameObject.GetComponent<PlayerController>();
-            Debug.Log("Retrieved player gameobject in flame controller.");
         }
 
     }
@@ -25,12 +42,11 @@ public class FlameController : MonoBehaviour
     public void OnTriggerExit2D(Collider2D collision)
     {
         playerController = null;
-        Debug.Log("Set playercontroller to null in Flame Controller.");
     }
 
     public void InteractWithFlame()
     {
-        if(flameData != null)
+        if(flameData != null &&  currentFlameState == FlameState.Lit)
         {
             UpdateFlameStaminaBar();
         }
@@ -38,10 +54,21 @@ public class FlameController : MonoBehaviour
 
     private void UpdateFlameStaminaBar()
     {
-        if(playerController != null)
-        {
-            //playerController.UpdateCurrentFlameStamina(flameData);
-        }
         updateFlameStaminaEvent?.RaiseEvent(flameData);
+
+        if(animator != null)
+        {
+            animator.SetBool("FlameOut", true);
+            currentFlameState = FlameState.Nonlit;
+            StartCoroutine(ResetFlame());
+        }
+    }
+
+    private IEnumerator ResetFlame()
+    {
+        yield return new WaitForSeconds(timeTilFlameRelights);
+        animator.SetBool("FlameOut", false);
+        currentFlameState = FlameState.Lit;
+        
     }
 }
